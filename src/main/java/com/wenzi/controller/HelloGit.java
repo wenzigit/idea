0783ffd7cfa009e42c136d67963c1fe72da99e38
wenzi <9423487@qq.com>
2019-06-30 15:27:33 +0800

    修改了主方法中输出的信息

--- src/main/java/com/wenzi/controller/HelloGit.java
+++ src/main/java/com/wenzi/controller/HelloGit.java
@@ -2,6 +2,6 @@ package com.wenzi.controller;
 
 public class HelloGit {
     public static void main(String[] args){
-      System.out.println("hello,git!");
+      System.out.println("hello,github!");
     }
 }
